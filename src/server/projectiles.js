0bd79e1809f397a3d95df4833a755fbaa57d1767
JoeTheBro22 <66972056+JoeTheBro22@users.noreply.github.com>
2021-11-11 19:38:19 -0800

    ;-;

--- src/server/projectiles.js
+++ src/server/projectiles.js
@@ -449,23 +449,25 @@ class Projectile {
 				break;
 			}
 			case "enemypusher": {
-				for(let i of Object.keys(enemies)){
-					const enemy = enemies[i];
-					if (Math.sqrt((this.x - enemy.x) ** 2 + (this.y - enemy.y) ** 2) < this.radius + enemy.radius + this.speed && !enemy.immune) {
-						if (enemy.deadTime < 1500){
-							enemy.deadTime = 1500;
+				if(Object.keys(enemies) !== undefined){
+					for(let i of Object.keys(enemies)){
+						const enemy = enemies[i];
+						if (Math.sqrt((this.x - enemy.x) ** 2 + (this.y - enemy.y) ** 2) < this.radius + enemy.radius + this.speed && !enemy.immune) {
+							if (enemy.deadTime < 1500){
+								enemy.deadTime = 1500;
+							}
+							if (enemy.disableTime < 2500){
+								enemy.disableTime = 2500;
+							}
+							  //enemies[i].x = this.x;
+							  //enemies[i].y = this.y;
+							  enemies[i].x += this.vx * delta / 30;
+							  enemies[i].y += this.vy * delta / 30;
+							  enemies[i].xChanged = true;
+							 enemies[i].yChanged = true;
 						}
-						if (enemy.disableTime < 2500){
-							enemy.disableTime = 2500;
-						}
-					  	//enemies[i].x = this.x;
-					  	//enemies[i].y = this.y;
-						  enemies[i].x += this.vx * delta / 30;
-						  enemies[i].y += this.vy * delta / 30;
-					  	enemies[i].xChanged = true;
-					 	enemies[i].yChanged = true;
 					}
-				  }
+				}
 				break;
 			}
 			case "thorn": {
